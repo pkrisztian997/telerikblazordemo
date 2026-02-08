9705b5f005100b8a357141c111570cae8854bf9e
pkrisztian997 <60325145+pkrisztian997@users.noreply.github.com>
2026-02-08 10:41:15 +0100

    Add project base
    * Add project base
    
    * Clean up code
    
    ---------
    
    Co-authored-by: Krisztian Porzsolt <porzsolt.krisztian@xpericad.com>

---
+++ SoftwareHouseDemo/Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareHouseDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherDto> Get()
+        {
+            var startDate = DateOnly.FromDateTime(DateTime.Now);
+            var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+            return Enumerable.Range(1, 15).Select(index => new WeatherDto
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            });
+        }
+    }
+
+    public class WeatherDto
+    {
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ SoftwareHouseDemo/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Components;
+using SoftwareHouseDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+AddTelerikBlazor(builder);
+AddControllerSupport(builder);
+
+var app = builder.Build();
+MapControllers(app);
+SetUpDebugErrorHandler(app);
+SetUpComponents(app);
+app.Run();
+
+static void AddTelerikBlazor(WebApplicationBuilder builder)
+{
+    builder.Services.AddTelerikBlazor();
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+}
+
+static void AddControllerSupport(WebApplicationBuilder builder)
+{
+    builder.Services.AddHttpClient();
+    builder.Services.AddControllers();
+    builder.Services.AddScoped(sp =>
+    {
+        var navigation = sp.GetRequiredService<NavigationManager>();
+        return new HttpClient { BaseAddress = new Uri(navigation.BaseUri) };
+    });
+}
+
+static void MapControllers(WebApplication app)
+{
+    app.UseRouting();
+    app.UseAntiforgery();
+    app.MapControllers();
+}
+
+static void SetUpDebugErrorHandler(WebApplication app)
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        app.UseHsts();
+    }
+}
+
+static void SetUpComponents(WebApplication app)
+{
+    app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+    app.UseHttpsRedirection();
+    app.MapStaticAssets();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+}
+
