c07dc45537ed4f7d861ef4b671008fd69a34b5ed
Krisztian Porzsolt <porzsolt.krisztian@xpericad.com>
2026-02-08 10:40:07 +0100

    Clean up code

--- SoftwareHouseDemo/Program.cs
+++ SoftwareHouseDemo/Program.cs
@@ -2,68 +2,55 @@ using Microsoft.AspNetCore.Components;
 using SoftwareHouseDemo.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTelerikBlazor();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-// -----
-builder.Services.AddHttpClient();
-builder.Services.AddControllers();
-builder.Services.AddScoped(sp =>
-{
-    var navigation = sp.GetRequiredService<NavigationManager>();
-    return new HttpClient { BaseAddress = new Uri(navigation.BaseUri) };
-});
-// -----
+AddTelerikBlazor(builder);
+AddControllerSupport(builder);
 
 var app = builder.Build();
-
-// -----
-app.UseRouting();
-app.UseAntiforgery();
-app.MapControllers();
-// -----
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
-}
-
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-app.MapStaticAssets();
-app.UseAntiforgery();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+MapControllers(app);
+SetUpDebugErrorHandler(app);
+SetUpComponents(app);
 app.Run();
 
-
-/*
- 
- using SoftwareHouseDemo.Components;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTelerikBlazor();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+static void AddTelerikBlazor(WebApplicationBuilder builder)
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
+    builder.Services.AddTelerikBlazor();
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
 }
 
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-app.MapStaticAssets();
-app.UseAntiforgery();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+static void AddControllerSupport(WebApplicationBuilder builder)
+{
+    builder.Services.AddHttpClient();
+    builder.Services.AddControllers();
+    builder.Services.AddScoped(sp =>
+    {
+        var navigation = sp.GetRequiredService<NavigationManager>();
+        return new HttpClient { BaseAddress = new Uri(navigation.BaseUri) };
+    });
+}
 
-app.Run();
+static void MapControllers(WebApplication app)
+{
+    app.UseRouting();
+    app.UseAntiforgery();
+    app.MapControllers();
+}
 
- */
+static void SetUpDebugErrorHandler(WebApplication app)
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        app.UseHsts();
+    }
+}
+
+static void SetUpComponents(WebApplication app)
+{
+    app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+    app.UseHttpsRedirection();
+    app.MapStaticAssets();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+}
+
